21d36344455684955ec9564c770f599c13701bf3
Alex Caron <alex.caron@gmail.com>
2023-06-06 13:38:06 -0500

    install mathlive for equations

--- client/src/components/Step.jsx
+++ client/src/components/Step.jsx
@@ -1,17 +1,15 @@
 import React, {useState} from 'react';
+import Editor from './Editor.jsx';
 
 const Step = () => {
   const [editing, setEditing] = useState(true);
   const [equation, setEquation] = useState('');
   return (
     <div className='step'>
+      <Editor active={editing} setEquation={setEquation}/>
       <div className='equation-content'>
-        {editing &&
-        <label>
-          Enter an equation:
-          <input value={equation} onChange={e => setEquation(e.target.value)}/>
-          <input type='submit' onClick={e => alert(equation)}/>
-        </label>}
+        <div>{equation}</div>
+        <math-field>f(x) = \sin(x+\pi)</math-field>
       </div>
       <div className='diagram-content'>
       </div>
