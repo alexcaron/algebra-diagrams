94dc9d8429f4858f9be8dc55a5bd709f1eb8b1b0
Alex Caron <alex.caron@gmail.com>
2023-06-06 15:53:20 -0500

    add styles and make button disabled when not needed

--- client/src/components/Step.jsx
+++ client/src/components/Step.jsx
@@ -4,12 +4,12 @@ import Diagram from './Diagram.jsx';
 
 const Step = () => {
   const [inputEquation, setInputEquation] = useState('');
-  const [displayEquation, setDisplayEquation] = useState('');
+  const [displayEquation, setDisplayEquation] = useState('["Equal",["Add",["Multiply",-3,"x"],10],22]');
   return (
     <div className='step'>
       <div className='equation-content'>
         <math-field onInput={e=>setInputEquation(e.target.getValue('math-json'))}>-3x+10 = 22</math-field>
-        <input type="submit" value="Go" onClick={e=>setDisplayEquation(inputEquation)}/>
+        <input type="submit" value="Go" onClick={e=>setDisplayEquation(inputEquation)} disabled={inputEquation===displayEquation}/>
       </div>
       <div className='diagram-content'>
         <Diagram equation={displayEquation}/>
