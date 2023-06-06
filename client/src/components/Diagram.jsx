02b8c7c871caf25c51e2d25353f3e579e8ee9833
Alex Caron <alex.caron@gmail.com>
2023-06-06 15:41:36 -0500

    create a button for displaying the equation in math-json

---
+++ client/src/components/Diagram.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Diagram = ({equation}) => {
+  return (
+    <div>{equation}</div>
+  );
+};
+
+export default Diagram;
--- client/src/components/Step.jsx
+++ client/src/components/Step.jsx
@@ -1,19 +1,18 @@
 import React, {useState} from 'react';
 import {ComputeEngine} from '@cortex-js/compute-engine';
-const Step = () => {
-  const [editing, setEditing] = useState(true);
-  const [equation, setEquation] = useState('');
-  const update = (e) => {
-    console.log(e.target.getValue('math-json'));
-  };
+import Diagram from './Diagram.jsx';
 
+const Step = () => {
+  const [inputEquation, setInputEquation] = useState('');
+  const [displayEquation, setDisplayEquation] = useState('');
   return (
     <div className='step'>
       <div className='equation-content'>
-        <math-field onInput={e=>update(e)}>3x+10 = 25</math-field>
+        <math-field onInput={e=>setInputEquation(e.target.getValue('math-json'))}>-3x+10 = 22</math-field>
+        <input type="submit" value="Go" onClick={e=>setDisplayEquation(inputEquation)}/>
       </div>
       <div className='diagram-content'>
-        <div>diagram here</div>
+        <Diagram equation={displayEquation}/>
       </div>
     </div>
   );
