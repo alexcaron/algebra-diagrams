4519bc417b1e3e2a8c9fa499a07ec7f8c424d528
Alex Caron <alex.caron@gmail.com>
2023-06-07 15:05:56 -0500

    finish tape and add step button

--- client/src/components/Step.jsx
+++ client/src/components/Step.jsx
@@ -1,23 +1,27 @@
 import React, {useState} from 'react';
 import {ComputeEngine} from '@cortex-js/compute-engine';
 import Diagram from './Diagram.jsx';
+const ce = new ComputeEngine();
 
-const Step = () => {
-  const defaultEquation = ["Equal",["Add",["Multiply",3,"x"],10],22];  // also hardcoded into math-field element
-  const [inputEquation, setInputEquation] = useState(defaultEquation);
-  const [displayEquation, setDisplayEquation] = useState(defaultEquation);
+const Step = ({equationStart = ["Equal",["Add",["Multiply",3,"x"],10],22]}) => {
+  const [inputEquation, setInputEquation] = useState(equationStart);
+  const [displayEquation, setDisplayEquation] = useState(equationStart);
+
   return (
+    <>
     <div className='step'>
       <div>
         <div className='equation-content'>
-          <math-field onInput={e=>setInputEquation(JSON.parse(e.target.getValue('math-json')))}>3x+10 = 22</math-field>
-          <input type="submit" value="Go" onClick={e=>setDisplayEquation(inputEquation)} disabled={inputEquation===displayEquation}/>
+          <math-field onInput={e=>setInputEquation(JSON.parse(e.target.getValue('math-json')))}>{ce.serialize(inputEquation)}</math-field>
+          <input type="button" value="Go" onClick={e=>setDisplayEquation(inputEquation)} disabled={inputEquation===displayEquation}/>
         </div>
       </div>
       <div className='diagram-content'>
         <Diagram equation={displayEquation}/>
       </div>
     </div>
+    <input className='add-step-button' type="button" value="Add step"/>
+    </>
   );
 }
 
